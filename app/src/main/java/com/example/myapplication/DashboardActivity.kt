cd4e352dd063ef017016d70e81d0a8d31e1558e1
Santicm23 <santicastrom@outlook.com>
2023-09-02 00:20:21 -0500

    creando todas las actividades

--- app/src/main/java/com/example/myapplication/DashboardActivity.kt
+++ app/src/main/java/com/example/myapplication/DashboardActivity.kt
@@ -1,11 +1,33 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityDashboardBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+
+    private lateinit var searchBtn: FloatingActionButton
+    private lateinit var profileBtn: FloatingActionButton
+    private lateinit var mapBtn: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        searchBtn = binding.buscarBtn
+        profileBtn = binding.perfilBtn
+        mapBtn = binding.mapaBtn
+
+        searchBtn.setOnClickListener {
+            val intent = Intent(applicationContext, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
