7e87f165846e2a329d7fa20d05fd7fab9c045c84
Santicm23 <santicastrom@outlook.com>
2023-09-01 23:02:24 -0500

    intents

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var loginBtn: Button
+    private lateinit var signInBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loginBtn = binding.loginBtn
+        signInBtn = binding.signInBtn
+
+        loginBtn.setOnClickListener {
+
+        }
+
+        signInBtn.setOnClickListener {
+            val intent = Intent(applicationContext, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/SignInActivity.kt
+++ app/src/main/java/com/example/myapplication/SignInActivity.kt
@@ -2,10 +2,31 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.myapplication.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+
+    private lateinit var loginBtn: Button
+    private lateinit var signInBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loginBtn = binding.backToLoginBtn
+        signInBtn = binding.createAccountBtn
+
+        loginBtn.setOnClickListener {
+            finish()
+        }
+
+        signInBtn.setOnClickListener {
+
+        }
     }
 }
