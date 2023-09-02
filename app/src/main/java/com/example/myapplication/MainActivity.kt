0baa97c809fbf23af3605d579b51aa2047dbe57c
Santicm23 <santicastrom@outlook.com>
2023-09-02 01:33:22 -0500

    corrigiendo error

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var loginBtn: Button
+    private lateinit var signInBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.MainActivity)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loginBtn = binding.loginBtn
+        signInBtn = binding.signInBtn
+
+        loginBtn.setOnClickListener {
+            val intent = Intent(applicationContext, DashboardActivity::class.java)
+            startActivity(intent)
+        }
+
+        signInBtn.setOnClickListener {
+            val intent = Intent(applicationContext, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
-
