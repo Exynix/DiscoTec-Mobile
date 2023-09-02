27efe9a593fb832e3ba3d60a239a81044b92e286
Santicm23 <santicastrom@outlook.com>
2023-09-02 10:39:53 -0500

    validación básica del login

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var loginBtn: Button
     private lateinit var signInBtn: Button
+    private lateinit var userInput: TextInputEditText
+    private lateinit var passInput: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +26,14 @@ class MainActivity : AppCompatActivity() {
 
         loginBtn = binding.loginBtn
         signInBtn = binding.signInBtn
+        userInput = binding.input0
+        passInput = binding.input1
 
         loginBtn.setOnClickListener {
-
-            val intent = Intent(applicationContext, DashboardActivity::class.java)
-            startActivity(intent)
+            if (userInput.text.toString().trim() != "" && passInput.text.toString().trim() != "") {
+                val intent = Intent(applicationContext, DashboardActivity::class.java)
+                startActivity(intent)
+            }
         }
 
         signInBtn.setOnClickListener {
