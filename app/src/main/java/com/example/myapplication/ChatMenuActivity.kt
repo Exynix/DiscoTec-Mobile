c79fe821f872da554b20c90c24879a3ac826c2f8
María Paula Cardona Rodríguez <mp.cardonar@javeriana.edu.co>
2023-10-18 11:34:37 -0500

    Backup commit ChatMenu

--- app/src/main/java/com/example/myapplication/ChatMenuActivity.kt
+++ app/src/main/java/com/example/myapplication/ChatMenuActivity.kt
@@ -3,7 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ChatMenu : AppCompatActivity() {
+class ChatMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_menu)
