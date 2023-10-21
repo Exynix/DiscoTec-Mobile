50e6d8c430529854d115602dc08504bd8439a66d
Arduousbook <davids_barretom@javeriana.edu.co>
2023-10-21 07:02:52 -0500

    Adicion de horizontal scroll en discoteca

--- app/src/main/java/com/example/myapplication/DashboardActivity.kt
+++ app/src/main/java/com/example/myapplication/DashboardActivity.kt
@@ -1,21 +1,36 @@
 package com.example.myapplication
 
+import android.R
+import android.app.ActionBar.LayoutParams
+import android.content.ContentValues.TAG
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.ResultReceiver
 import android.util.Log
+import android.util.TypedValue
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginEnd
+import androidx.core.view.marginRight
+import androidx.core.view.setMargins
+import com.bumptech.glide.Glide
 import com.example.myapplication.databinding.ActivityDashboardBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kotlin.math.log
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+
 
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDashboardBinding
+
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,7 +67,73 @@ class DashboardActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // Logica para la conexión a la realtime database, y lectura de su información
+        val database = Firebase.database
+        val myRef = database.getReference("discotecas")
+
+        myRef.get()
+        val discotecas = ArrayList<Discoteca>()
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (discotecaSnapshot in dataSnapshot.children) {
+                    // Create a new Discoteca object
+                    val discoteca = Discoteca()
+
+                    // Set the values of the Discoteca object
+                    discoteca.idDiscoteca = discotecaSnapshot.child("id").getValue(Long::class.java)!!
+
+                    discoteca.nombre =
+                        discotecaSnapshot.child("nombre").getValue(String::class.java).toString()
+
+                    discoteca.precioCover = discotecaSnapshot.child("precio_cover").getValue(Float::class.java)!!
+
+                    discoteca.ubicacion =
+                        discotecaSnapshot.child("ubicacion").getValue(String::class.java).toString()
+
+                    discoteca.descripcion =
+                        discotecaSnapshot.child("descripcion").getValue(String::class.java).toString()
+
+                    discotecas.add(discoteca)
+
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException())
+            }
+
+        })
+
+        // Lectura de la información de cloud storage. Usado para las imágenes.
+        val storage = Firebase.storage
+        val storageReference = FirebaseStorage.getInstance().reference.child("fotos_discotecas")
+
+        storageReference.listAll().addOnSuccessListener{listResult ->
+
+            val linearLayout: LinearLayout = binding.horizontalLinearLayout
+            for (item in listResult.items) {
+                item.downloadUrl.addOnSuccessListener{
+                        imageUrl ->
+                    // Add the image to the horizontal scroll view
+                    val imageView: ImageView = ImageView(this)
+
+                    Glide.with(this).load(imageUrl.toString()).into(imageView)
+
+                    linearLayout.addView(imageView)
+                    val layoutParams = LinearLayout.LayoutParams(500, LinearLayout.LayoutParams.MATCH_PARENT)
+                    layoutParams.setMargins(50)
+                    imageView.layoutParams = layoutParams
+                    imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+                    imageView.requestLayout()
+                }
+            }
+        }
+
+
     }
+
 }
 
 
