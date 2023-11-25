035a386166e1bea2011ef582297ba734386b556f
Laura Ovalle Benítez <ovalle.laura@javeriana.edu.co>
2023-11-25 00:41:13 -0500

    Error con el adapter

--- app/src/main/java/com/example/myapplication/model/User.kt
+++ app/src/main/java/com/example/myapplication/model/User.kt
@@ -1,16 +1,15 @@
 package com.example.myapplication.model
 
-import com.example.myapplication.lookupTableClasses.UserType
-import com.google.firebase.auth.FirebaseUser
-import java.sql.Timestamp
+class User {
+    var key: String? = null
+    var nombre: String = ""
+    var nroId: Long = 0
+    var correo: String = ""
 
-class User constructor(
-    firebaseUser: FirebaseUser,
-    name: String,
-    lastName: String,
-    idNumber: Number,
-    birthdate: Timestamp,
-    nightcoins: Float,
-    userType: UserType
-){
+    constructor()
+    constructor(nombre: String, nroId: Long, correo: String) {
+        this.nombre = nombre
+        this.nroId = nroId
+        this.correo = correo
+    }
 }
