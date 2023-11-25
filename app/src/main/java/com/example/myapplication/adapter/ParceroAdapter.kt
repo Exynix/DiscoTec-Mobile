5711f83918855f79b8c4792ed420cd5cca0b420d
Santicm23 <santicastrom@outlook.com>
2023-11-24 19:31:27 -0500

    Adapter parceros

---
+++ app/src/main/java/com/example/myapplication/adapter/ParceroAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+import org.json.JSONObject
+
+class ParceroAdapter(private val parceros: List<JSONObject>) : RecyclerView.Adapter<ParceroViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ParceroViewHolder {
+        val layouteInflater = LayoutInflater.from(parent.context)
+        return ParceroViewHolder(layouteInflater.inflate(R.layout.info_usuario, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ParceroViewHolder, position: Int) {
+        holder.render(parceros[position])
+    }
+
+    override fun getItemCount(): Int = parceros.size
+
+}
